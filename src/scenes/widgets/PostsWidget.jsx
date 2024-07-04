187195b93e6464cfe23a10d8dad5aef0b1aa4a2c
Olivrrcode <o.atherton@outlook.com>
2024-07-04 12:13:22 +0100

    changed all fetch calls to fetch from the correct source

--- src/scenes/widgets/PostsWidget.jsx
+++ src/scenes/widgets/PostsWidget.jsx
@@ -10,17 +10,20 @@ const PostsWidget = ({ userId, isProfile = false }) => {
   const token = useSelector((state) => state.token);
 
   const getPosts = async () => {
-    const response = await fetch('http://localhost:3001/posts', {
-      method: 'GET',
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    const response = await fetch(
+      'https://mern-social-media-backend-kh5p.onrender.com/posts',
+      {
+        method: 'GET',
+        headers: { Authorization: `Bearer ${token}` },
+      },
+    );
     const data = await response.json();
     dispatch(setPosts({ posts: data }));
   };
 
   const getUserPosts = async () => {
     const response = await fetch(
-      `http://localhost:3001/posts/${userId}/posts`,
+      `https://mern-social-media-backend-kh5p.onrender.com/posts/${userId}/posts`,
       {
         method: 'GET',
         headers: { Authorization: `Bearer ${token}` },
